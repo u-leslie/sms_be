747c3743d29c4144e870e8b9a206c94ee6a8e16b
u-leslie <heavenmadeleslie@gmail.com>
2024-11-18 07:46:59 +0200

    authentication

--- network/forms.py
+++ network/forms.py
@@ -1,9 +1,7 @@
-# forms.py
-
 from django import forms
-from .models import User  # Assuming you have a UserProfile model for storing user info
+from .models import User
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
-        model = User  # Your model for the user profile
-        fields = ['username','first_name', 'last_name', 'profile_pic', 'cover']  # Add any other fields you want to allow editing
+        model = User
+        fields = ['username','first_name', 'last_name', 'profile_pic', 'cover'] 
